1dbaa92bc61b5c946cd85014827efaea2a41c9e7
Spencer Lott <spklott@gmail.com>
2023-09-25 20:55:09 -0400

    saving for the night

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './App.css';
 
 import Header from './components/header/Header';
@@ -6,6 +6,14 @@ import Sidebar from './components/sidebar/Sidebar';
 import Home from './components/home/Home';
 
 import cx from 'classnames'
+
+// export const mainRef = useRef(null);
+// export const aboutRef = useRef(null);
+// export const projectsRef = useRef(null);
+
+// export const scrollToMain = () => mainRef.current.scrollIntoView({ behavior: "smooth" });
+// export const scrollToAbout = () => aboutRef.current.scrollIntoView({ behavior: "smooth" });
+// export const scrollToContact = () => projectsRef.current.scrollIntoView({ behavior: "smooth", block: "end" });
 
 function App() {
   const [toggle, setToggle] = useState(false)
