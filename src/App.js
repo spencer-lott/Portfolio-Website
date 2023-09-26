23d83dfc7836272f43e74b29e82e0d4072010d69
Spencer Lott <spklott@gmail.com>
2023-09-26 18:51:52 -0400

    got rid of the sidebar and condensed it all into App.js

--- src/App.js
+++ src/App.js
@@ -1,53 +1,74 @@
-import React from 'react';
-import { useState } from 'react';
-import './App.css';
-import './Sidebar.css';
-
-import Header from './components/header/Header';
-import Home from './components/home/Home';
-
-import cx from 'classnames';
+import React from 'react'
+import { useState } from 'react'
+import './App.css'
+import { useRef } from "react"
+import {Container, Row} from "reactstrap"
 
 function App() {
-  const [toggle, setToggle] = useState(false);
 
-  const classes = cx(
-    'pusher',
-    'bottom',
-    { 'dimmed': toggle }
-  );
+  const aboutRef = useRef(null)
+  const projectsRef = useRef(null)
 
-  const toggleMenu = () => {
-    setToggle(!toggle)
-  };
-
-  const semantics = cx(
-    'ui', 'sidebar', 'overlay', 'left', 'inverted', 'menu', 'animating',
-    { 'visible': toggle }
-  )
+  const scrollToAbout = () => aboutRef.current.scrollIntoView({ behavior: "smooth" });
+  const scrollToProjects = (e) => {
+    e.preventDefault();
+    projectsRef.current.scrollIntoView({ behavior: "smooth" })
+  }
 
   return (
     <div className="App">
-      <Header onToggleMenu={toggleMenu} />
 
       <div className='ui attached pushable' >
-        <div className={semantics}>
-          <ul>
-            <li className='item link'>-----</li>
-            <li className='item link'>-----</li>
-            <li className='item link'>About Me</li>
-            <li className='item link'>Projects</li>
-            <li className='item link'>Accomplishments</li>
-            <li className='item link'>Interests</li>
-            <li className='item link'>Family Life</li>
-          </ul>
-        </div>
-        <div className={classes}>
-          <Home />
+            <div className="main">
+                  <div className='home'>
+                  <Container>
+                      <Row>
+                          <div className="home-top">
+                          <h1 className="name-header">Spencer Lott</h1>
+                              <div className="img-container">
+                                  <img className="profile-img" src="/images/Spencer-Lott-Headshot-Copy-SQ.jpeg" alt="no image"/>
+                              </div>
+                          </div>
+                      </Row>
+                      <Row>
+                          <div className="info-bar">
+                              <div className="info-bar-details">
+                                  <h2>Junior Full-Stack Developer</h2>
+                                  <div>
+                                      <h3 className="h3-details"><i className="bi bi-pin-map-fill"></i> Huntington-Charleston, WV area |
+                                          <a href="https://github.com/spencer-lott" style={{color: "#000"}}><i className="bi bi-github"></i> Github</a> |
+                                          <a href="https://www.linkedin.com/in/spencer-k-lott/"><i className="bi bi-linkedin"></i>LinkedIn</a>
+                                      </h3>
+                                  </div>
+                              </div>
+                          </div>
+                      </Row>
+                      <Row>
+                          <section className="projects-container" ref={projectsRef}>
+                              <div className="prj-img-container">
+                                  <h1 className="prj-header">Bird Bud</h1>
+                                  <a href=""><img className="thumbnail" src="/images/BirdBud-Thumbnail.png" alt=""/></a>
+                                  <footer className="prj-footer">
+                                      <p><i>Front-End Capstone Project</i></p>
+                                  <a href="https://github.com/spencer-lott/bird-capstone"><p className="ghub-ptag"><i className="bi bi-github"></i>Github Link</p></a>
+                                  </footer>
+                              </div>
+                              <div className="prj-img-container">
+                                  <h1 className="prj-header">Property Manager</h1>
+                                  <a href="https://www.loom.com/share/0c9bc0b8eae34a289396a8812156d659"><img className="thumbnail" src="/images/PropertyManager-Thumbnail.png" alt=""/></a>
+                                  <footer className="prj-footer">
+                                      <p><i>Full-Stack Capstone Project</i></p>
+                                  <a href="https://github.com/spencer-lott/property-capstone"><p className="ghub-ptag"><i className="bi bi-github"></i>Github Link</p></a>
+                                  </footer>
+                              </div>
+                          </section>
+                      </Row>
+                  </Container>
+                </div>
+            </div>
         </div>
       </div>
-    </div>
-  );
+  )
 }
 
 export default App;
