c19e72503fe6fcdd077d1740bc47a514f314dac3
Spencer Lott <spklott@gmail.com>
2023-09-26 18:17:58 -0400

    saving changes

--- src/App.js
+++ src/App.js
@@ -1,44 +1,52 @@
-import { useRef, useState } from 'react';
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
+import './Sidebar.css';
 
 import Header from './components/header/Header';
-import Sidebar from './components/sidebar/Sidebar';
 import Home from './components/home/Home';
 
-import cx from 'classnames'
-
-// export const mainRef = useRef(null);
-// export const aboutRef = useRef(null);
-// export const projectsRef = useRef(null);
-
-// export const scrollToMain = () => mainRef.current.scrollIntoView({ behavior: "smooth" });
-// export const scrollToAbout = () => aboutRef.current.scrollIntoView({ behavior: "smooth" });
-// export const scrollToContact = () => projectsRef.current.scrollIntoView({ behavior: "smooth", block: "end" });
+import cx from 'classnames';
 
 function App() {
-  const [toggle, setToggle] = useState(false)
+  const [toggle, setToggle] = useState(false);
 
   const classes = cx(
     'pusher',
     'bottom',
-    {'dimmed' : toggle}
-  )
+    { 'dimmed': toggle }
+  );
 
   const toggleMenu = () => {
     setToggle(!toggle)
-  }
+  };
+
+  const semantics = cx(
+    'ui', 'sidebar', 'overlay', 'left', 'inverted', 'menu', 'animating',
+    { 'visible': toggle }
+  )
 
   return (
     <div className="App">
       <Header onToggleMenu={toggleMenu} />
 
       <div className='ui attached pushable' >
-        <Sidebar toggleMenu={toggle} />
-          <div className={classes}>
-            <Home />
-          </div>
+        <div className={semantics}>
+          <ul>
+            <li className='item link'>-----</li>
+            <li className='item link'>-----</li>
+            <li className='item link'>About Me</li>
+            <li className='item link'>Projects</li>
+            <li className='item link'>Accomplishments</li>
+            <li className='item link'>Interests</li>
+            <li className='item link'>Family Life</li>
+          </ul>
+        </div>
+        <div className={classes}>
+          <Home />
+        </div>
       </div>
-</div>
+    </div>
   );
 }
 
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,24 +1,24 @@
-import cn from 'classnames'
+// import cn from 'classnames'
 
-export default function Sidebar({toggleMenu}) {
+// export default function Sidebar({toggleMenu}) {
 
-const classes = cn(
-  'ui', 'sidebar', 'overlay', 'left', 'inverted', 'menu', 'animating', {'visible' : toggleMenu}
-)
+// const classes = cn(
+//   'ui', 'sidebar', 'overlay', 'left', 'inverted', 'menu', 'animating', {'visible' : toggleMenu}
+// )
 
-  return (
-    <div className={classes}>
-        <ul>
-            <li className='item link'>-----</li>
-            <li className='item link'>-----</li>
-            <li className='item link'>-----</li>
-            <li className='item link'>About Me</li>
-            <li className='item link'>Projects</li>
-            <li className='item link'>Accomplishments</li>
-            <li className='item link'>Interests</li>
-            <li className='item link'>Family Life</li>
-        </ul>
+//   return (
+//     <div className={classes}>
+//         <ul>
+//             <li className='item link'>-----</li>
+//             <li className='item link'>-----</li>
+//             <li className='item link'>-----</li>
+//             <li className='item link'>About Me</li>
+//             <li className='item link'>Projects</li>
+//             <li className='item link'>Accomplishments</li>
+//             <li className='item link'>Interests</li>
+//             <li className='item link'>Family Life</li>
+//         </ul>
         
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
